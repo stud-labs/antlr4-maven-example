b35b217db95ebe96c6ce61686d8903ad09ae8e85
Evgeny Cherkashin <eugeneai@irnok.net>
2023-10-10 09:42:04 +0800

    Really added new main implementation.

--- src/main/java/org/antlrfun/Hello.java
+++ src/main/java/org/antlrfun/Hello.java
@@ -1,16 +1,17 @@
 package org.antlrfun;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
+import java.nio.charset.StandardCharsets;
 
 public class Hello {
     public static void main(String [] args) throws Exception
     {
-
-        ANTLRInputStream antlrInputStream = new ANTLRInputStream("hello world");
-
-        HelloLexer lexer = new HelloLexer(antlrInputStream);
-
+        String input = "hello world";
+        InputStream stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
+        HelloLexer lexer = new HelloLexer(CharStreams.fromStream(stream, StandardCharsets.UTF_8));
         CommonTokenStream tokens = new CommonTokenStream( lexer );
         HelloParser parser = new HelloParser( tokens );
         ParseTree tree = parser.r();
